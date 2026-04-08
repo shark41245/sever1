4e5aa46ac05ad9592810c752bbbb70cafb78b9b8
shark41245 <machines0101@naver.com>
2026-04-08 09:17:56 +0900

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,3 @@
-
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { appRouter } from "../server/routers.js";
@@ -7,10 +6,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/trpc", createExpressMiddleware({
-  router: appRouter,
-  createContext: () => ({})
-}));
+app.use(
+  "/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext: () => ({}),
+  })
+);
 
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
